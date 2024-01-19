1a2e17a036387e74aed26c192f5d432cfa903e22
dixhasharma <dixhusharma@gmail.com>
2024-01-19 16:00:12 +0530

    "gvj"

---
+++ src/components/Home/Index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+    <div>Index</div>
+     
+  )
+}
+
+export default Index
---
+++ src/components/Route1.jsx
@@ -0,0 +1,13 @@
+const Route1=()=>{
+    return(
+        <nav>
+            <ul>
+                <li><a href=""></a>home</li>
+                <li>home</li>
+                <li><a href=""></a>home</li>
+                <li><a href=""></a>home</li>
+            </ul>
+        </nav>
+    )
+}
+export default Route1
